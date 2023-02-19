ace743c9ccfb96ff4b574863171d877f429a586a
Arpit Bharti <arpitbharti73@gmail.com>
2023-02-19 16:25:39 +0530

    traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,14 +10,17 @@
 // No boiler plate code this time,
 // you can do this!
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-// TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut g = self.to_vec();
+        g.push(String::from("Bar"));
+        g
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -26,6 +29,7 @@ mod tests {
     #[test]
     fn is_vec_pop_eq_bar() {
         let mut foo = vec![String::from("Foo")].append_bar();
+
         assert_eq!(foo.pop().unwrap(), String::from("Bar"));
         assert_eq!(foo.pop().unwrap(), String::from("Foo"));
     }
